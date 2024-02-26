279ae8201c7f9fc35094897f2d785bb76fc9ae03
Soumyajit <87481763+soumyajit0@users.noreply.github.com>
2024-02-27 02:08:54 +0530

    Add files via upload

--- Assignment 1/Message Queue 1/hex.c
+++ Assignment 1/Message Queue 1/hex.c
@@ -6,27 +6,32 @@
 #include<sys/ipc.h>
 #include<sys/msg.h>
 
-struct message_struct{
-	long int message_type;
-	char message_body[BUFSIZ];
+struct message_struct
+{
+  long int message_type;
+  char message_body[BUFSIZ];
 };
 
-int main(){
-	int running=1;
-	
-	int msgid=msgget((key_t)2832,0666|IPC_CREAT);
-	
-	struct message_struct message;
-	
-	while(running==1){
-		msgrcv(msgid,(void*)&message,BUFSIZ,3,0);
-		if (strcmp(message.message_body,"end")==0){
-			running=0;
-			printf("\nProcess Terminated...\n");
-			continue;
-		}
-		printf("%s\n",message.message_body);
-	}
+int
+main ()
+{
+  int running = 1;
 
-	return 0;	
+  int msgid = msgget ((key_t) 2832, 0666 | IPC_CREAT);
+
+  struct message_struct message;
+
+  while (running == 1)
+    {
+      msgrcv (msgid, (void *) &message, BUFSIZ, 3, 0);
+      if (strcmp (message.message_body, "end") == 0)
+	{
+	  running = 0;
+	  printf ("\nProcess Terminated...\n");
+	  continue;
+	}
+      printf ("%s\n", message.message_body);
+    }
+
+  return 0;
 }
