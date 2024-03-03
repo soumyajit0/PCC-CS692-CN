b8cf3c62fc35e3f98fe08a1e6e9092e3e14c8e52
Soumyajit <87481763+soumyajit0@users.noreply.github.com>
2024-03-03 16:19:32 +0530

    Add files via upload

---
+++ PCA 1/Set B/client.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/stat.h>
+#include<sys/un.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+
+#define MAX 100
+
+int main(){
+	struct sockaddr_un address;
+	int sockfd=socket(AF_UNIX,SOCK_STREAM,0);
+	address.sun_family=AF_UNIX;
+	strcpy(address.sun_path,"socket_server");
+	int len=sizeof(address);
+	int result=connect(sockfd,(struct sockaddr*)&address,len);
+	if (result==-1){
+		printf("Cannot connect to Server...\n");
+		exit(1);
+	}
+	printf("Connected to Server...\n\n");
+	while(1){
+		char ip[MAX];
+		printf("Enter an IPv4 Address : ");
+		scanf("%s",ip);
+		getchar();
+		write(sockfd,ip,strlen(ip)+1);
+		if (strcmp(ip,"end")==0){
+			printf("Client is terminated...\n");
+			break;
+		}
+		printf("Client sent IPv4 : %s to the Server\n",ip);
+		char valid[MAX];
+		read(sockfd,valid,MAX);
+		if (strcmp(valid,"1")==0)
+			printf("IPv4 : %s is VALID\n\n",ip);
+		else
+			printf("IPv4 : %s is INVALID\n\n",ip);
+	}
+	close(sockfd);
+	return 0;
+}
---
+++ PCA 1/Set B/server.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/stat.h>
+#include<sys/un.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+
+#define MAX 100
+
+int isValid(char* ip){
+	int sum=0,count=0,i;
+	for(i=0;i<strlen(ip);i++){
+		if (ip[i]=='.'){
+			if (sum>255 || count>=3 || (i!=0 && ip[i-1]=='.'))
+				return 0;
+			count++;
+			sum=0;
+		}
+		else{
+			sum=sum*10+ip[i]-48;
+		}
+	}
+	if (count!=3)	return 0;
+	return 1;
+}
+
+int main(){
+	struct sockaddr_un client_address,server_address;
+	int server_sockfd=socket(AF_UNIX,SOCK_STREAM,0);
+	server_address.sun_family=AF_UNIX;
+	strcpy(server_address.sun_path,"socket_server");
+	int server_len=sizeof(server_address);
+	bind(server_sockfd,(struct sockaddr*)&server_address,server_len);
+	listen(server_sockfd,5);
+	printf("Server is waiting...\n\n");
+	int client_len=sizeof(client_address);
+	int client_sockfd=accept(server_sockfd,(struct sockaddr*)&client_address,&client_len);
+	while(1){
+		char ip[MAX];
+		read(client_sockfd,ip,MAX);
+		if (strcmp(ip,"end")==0){
+			printf("Server is terminated...\n");
+			break;
+		}
+		printf("Server received IPv4 : %s from the Client\n",ip);
+		char valid[MAX];
+		if (isValid(ip)==1)
+			strcpy(valid,"1");
+		else
+			strcpy(valid,"0");
+		write(client_sockfd,valid,strlen(valid)+1);
+		printf("Server analyzed the IP and sent its judgement to the Client\n\n");
+	}
+	close(server_sockfd);
+	return 0;
+}
