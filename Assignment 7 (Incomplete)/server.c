aac866f93c3f2aa629f3fa931c22e5616129ed8c
Soumyajit <87481763+soumyajit0@users.noreply.github.com>
2024-04-27 22:07:19 +0530

    Add files via upload

--- Assignment 7 (Incomplete)/server.c
+++ Assignment 7 (Incomplete)/server.c
@@ -12,6 +12,59 @@
 
 #define MAX 100
 
+char *
+hammingCode (char *data)
+{
+  int m = strlen (data);
+  int i, j, r = 1, k = 0, count = 0, shift = 0;
+  for (i = 0; i < m / 2; i++)
+    {
+      char temp = data[i];
+      data[i] = data[m - 1 - i];
+      data[m - 1 - i] = temp;
+    }
+
+  while ((1 << r < (m + r + 1)))
+    {
+      r++;
+    }
+
+  char *code = (char *) malloc (sizeof (char) * (m + r + 1));
+  code[m + r] = '\0';
+
+  for (i = 0; i < m + r; i++)
+    {
+      if ((i + 1) & i)
+	code[i] = data[k++];
+      else
+	code[i] = 'r';
+    }
+
+  for (i = 0; i < r; i++)
+    {
+      count = 0;
+      shift = 1 << i;
+      for (j = 1; j <= (m + r); j++)
+	{
+	  if (j & shift)
+	    {
+	      if (code[j - 1] == '1')
+		count++;
+	    }
+	}
+      code[shift - 1] = '0' + (count & 1);
+    }
+  int Hm = strlen (code);
+  for (i = 0; i < Hm / 2; i++)
+    {
+      char temp = code[i];
+      code[i] = code[Hm - 1 - i];
+      code[Hm - 1 - i] = temp;
+    }
+
+  return code;
+}
+
 int
 main (int ac, char **av)
 {
@@ -38,7 +91,7 @@ main (int ac, char **av)
 	  exit (0);
 	}
       printf ("Dataword %s received from the client\n", data);
-      strcpy (code, data);
+      strcpy (code, hammingCode (code));
       sendto (sid, (void *) &code, strlen (code), 0,
 	      (struct sockaddr *) &caddr, sizeof (caddr));
       printf ("Codeword %s returned to the client\n\n", code);
