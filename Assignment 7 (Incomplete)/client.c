79f131372cd62bb6856ebb38f114d1ae89b185f0
Soumyajit Dey Sarkar <87481763+soumyajit0@users.noreply.github.com>
2024-05-25 22:36:41 +0530

    Update client.c

--- Assignment 7 (Incomplete)/client.c
+++ Assignment 7 (Incomplete)/client.c
@@ -1,48 +1,36 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-
 #include<netinet/in.h>
 #include<arpa/inet.h>
-
 #include<sys/types.h>
 #include<sys/socket.h>
-
 #include<unistd.h>
 
 #define MAX 100
 
-int
-main (int ac, char **av)
-{
-  struct sockaddr_in saddr;
-  char sip_addr[MAX], data[MAX], code[MAX];
-  if (ac == 1)
-    strcpy (sip_addr, "127.0.0.1");
-  else
-    strcpy (sip_addr, av[1]);
-  int sid = socket (AF_INET, SOCK_DGRAM, 0);
-  saddr.sin_family = AF_INET;
-  saddr.sin_addr.s_addr = inet_addr (sip_addr);
-  saddr.sin_port = htons (1234);
-  while (1)
-    {
-      printf ("Enter the Dataword :");
-      scanf ("%s", data);
-      getchar ();
-      sendto (sid, (void *) &data, strlen (data) + 1, 0,
-	      (struct sockaddr *) &saddr, sizeof (saddr));
-      if (strcmp (data, "end") == 0)
-	{
-	  printf ("Client terminated...\n");
-	  close (sid);
-	  exit (0);
+void main(int argc,char ** argv){
+	if(argc<3){
+		printf("provide IP and port no!");
+		exit(1);
 	}
-      printf ("Client sent Dataword %s to the server\n", data);
-      int len = sizeof (saddr);
-      recvfrom (sid, (void *) &code, MAX, 0, (struct sockaddr *) &saddr,
-		&len);
-      printf ("Client received Codeword %s from the server\n\n", code);
-    }
-  return 0;
+	
+	struct sockaddr_in server;
+	server.sin_family=AF_INET;
+	server.sin_port=htons(atoi(argv[2]));
+	server.sin_addr.s_addr=inet_addr(argv[1]);
+	
+	int sockfd=socket(AF_INET,SOCK_DGRAM,0);
+		
+	socklen_t len=sizeof(server);
+	char buff[512],result[512];
+	while(1){
+		bzero(buff,sizeof(buff));
+		printf("Enter data: ");
+		scanf(" %256[01][^\n]s",buff);
+		sendto(sockfd,buff,sizeof(buff),0,(struct sockaddr*)&server,sizeof(server));
+
+		recvfrom(sockfd,result,sizeof(result),0,(struct sockaddr*)&server,&len);
+		printf("Code word received: %s\n",result);
+	}
 }
