e940118bfe50666927c2d686fe14d7cc46112f24
Soumyajit Dey Sarkar <87481763+soumyajit0@users.noreply.github.com>
2024-05-26 19:49:11 +0530

    Update client.c

--- Assignment 12/client.c
+++ Assignment 12/client.c
@@ -1,77 +1,89 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <pthread.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <time.h>
-#define SIZE 512
-#define TIMEOUT 4
-#define error(msg) {perror(msg);printf("\n");exit(1);}
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<arpa/inet.h>
+#include<netinet/in.h>
+#include<time.h>
 
-typedef struct
+#define MAX 100
+#define TIMEOUT 2
+
+struct frame_struct
 {
   unsigned int seq_num;
-  char data[SIZE];
-} Frame;
+  char frame_body[MAX];
+};
 
-void
-main (int argc, char **argv)
+int main(int argc,char **argv)
 {
-  if (argc < 3)
+  if(argc<2)
+  {
+    printf("Please provide the IP and Port...\n");
+    exit(1);
+  }
+  char sip_addr[MAX];
+  strcpy(sip_addr,argv[1]);
+  int port=atoi(argv[2]);
+  int sid=socket(AF_INET,SOCK_STREAM,0);
+  if(sid<0)
+  {
+    printf("Error while creating the Socket...\n");
+    exit(1);
+  }
+  struct timeval tv;
+  tv.tv_sec=TIMEOUT;
+  tv.tv_usec=0;
+  if(setsockopt(sid,SOL_SOCKET,SO_RCVTIMEO,&tv,sizeof(tv))<0)
+  {
+    printf("Error while setting Socket options...\n");
+    close(sid);
+    exit(1);
+  }
+  struct sockaddr_in saddr;
+  saddr.sin_family=AF_INET;
+  saddr.sin_addr.s_addr=inet_addr(sip_addr);
+  saddr.sin_port=htons(port);
+  if(connect(sid,(struct sockaddr*)&saddr,sizeof(saddr))<0)
+  {
+    printf("Error while connecting to the Server...\n");
+    close(sid);
+    exit(1);
+  }
+  printf("connect: Success\n");
+  int seq_no=0,ack_rcv;
+  int timer=1;
+  int ack=1;
+  char *data="Aopz jvkl pz jvwplk myvt Zvbtfhqpa";
+  while(1)
+  {
+    struct frame_struct frame;
+    frame.seq_num=seq_no;
+    strcpy(frame.frame_body,data);
+    send(sid,(void *)&frame,sizeof(frame),0);
+    if(ack==1)
+      printf("\nSending Sequence number: %d\n",seq_no);
+    else
     {
-      printf ("Provide IP and port!\n");
-      exit (1);
+      printf("\nRe-Sending Sequence number: %d\n",seq_no);
+      ack=1;
     }
-
-  struct sockaddr_in server = {
-    .sin_family = AF_INET,
-    .sin_port = htons (atoi (argv[2])),
-    .sin_addr.s_addr = inet_addr (argv[1]),
-  };
-
-  struct timeval tv = {
-    .tv_sec = TIMEOUT,
-    .tv_usec = 0,
-  };
-
-  int sockfd = socket (AF_INET, SOCK_STREAM, 0);
-  if (sockfd < 0)
-    error ("socket creation failed");
-
-  int res = setsockopt (sockfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof (tv));
-  if (res < 0)
-    error ("Setsockopt Failed!");
-
-  res = connect (sockfd, (struct sockaddr *) &server, sizeof (server));
-  if (res < 0)
-    error ("Failed to accept connection!");
-
-  int seq_no, ack_no;
-
-  Frame frame;
-
-  char *data = "Hello this is some exaple frame data";
-  seq_no = 0;
-  int ack_recv = 0, bytesRead;
-  while (seq_no < 5)
+    printf("Started Timer %d\n",timer++);
+    int bytesRead=recv(sid,(void *)&ack_rcv,sizeof(ack_rcv),0);
+    if(bytesRead<=0||ack_rcv!=seq_no+1)
     {
-      frame.seq_num = seq_no;
-      strcpy (frame.data, data);
-      send (sockfd, &frame, sizeof (frame), 0);
-      printf ("\nTimer for frame no %d started...\n", seq_no);
-      bytesRead = recv (sockfd, &ack_recv, sizeof (int), 0);
-      if (bytesRead <= 0 || ack_recv != seq_no + 1)
-	{
-	  printf ("Failed to get acknowledgement for frame %d...\n", seq_no);
-	  printf ("Initiating retransmission...");
-	}
-      else
-	{
-	  printf ("Acknowledgement for frame %d received..\n", seq_no++);
-	}
+      printf("Acknowledgement not Received. Timer timed out.\n");
+      ack=0;
     }
-
+    else
+    {
+      printf("Acknowledgment %d Received\n",ack_rcv);
+      seq_no++;
+      ack=1;
+    }
+  }
+  close(sid);
+  return 0;
 }
