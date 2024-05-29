ecaf61b95c87fd98e9765775ddb3bf42355c5f54
Soumyajit Dey Sarkar <87481763+soumyajit0@users.noreply.github.com>
2024-05-29 18:02:56 +0530

    Add files via upload

---
+++ Assignment 3/UDP/server.c
@@ -0,0 +1,120 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<netinet/in.h>
+#include<fcntl.h>
+#include<math.h>
+
+#define MAX 100
+#define int long long int
+
+struct message_struct
+{
+  char dataword[MAX];
+  char divisor[MAX];
+  char codeword[MAX];
+  char remainder[MAX];
+};
+
+void reverse(char *str)
+{
+  int i=0,j=strlen(str)-1;
+  while(i<j)
+  {
+    char temp=str[i];
+    str[i]=str[j];
+    str[j]=temp;
+    i++;
+    j--;
+  }
+}
+
+int toDec(char *str)
+{
+  int i,power=strlen(str)-1,n=0;
+  for(i=0;i<strlen(str);i++)
+  {
+    n+=pow(2,power)*(str[i]-48);
+    power--;
+  }
+  return n;
+}
+
+void toBin(int dec,char *bin)
+{
+  int i=0;
+  while(dec>0)
+  {
+    bin[i++]=(char)(dec%2+48);
+    dec/=2;
+  }
+  bin[i]='\0';
+  reverse(bin);
+}
+
+void CRC(char *datawordBin,char *divisorBin,char *codewordBin,char *remainderBin)
+{
+  int divisorLen=strlen(divisorBin);
+  int datawordDec=toDec(datawordBin);
+  int divisorDec=toDec(divisorBin);
+  int dividendDec=datawordDec << (divisorLen-1);
+  int shft=(int)(ceil(log2(dividendDec+1))-divisorLen);
+  while(dividendDec>=divisorDec || shft>=0)
+  {
+    int remDec=(dividendDec>>shft)^divisorDec;
+    dividendDec=(dividendDec&((1ll<<shft)-1))|(remDec<<shft);
+    shft=(int)(ceil(log2(dividendDec+1))-divisorLen);
+  }
+  int codewordDec=(datawordDec<<(divisorLen-1))|dividendDec;
+  toBin(codewordDec,codewordBin);
+  strcpy(remainderBin,codewordBin+strlen(datawordBin));
+}
+
+int main(int argc,char **argv)
+{
+  char sip_addr[MAX];
+  int port;
+  if(argc!=3)
+  {
+    printf("Please provide IP and Port No....\n");
+    exit(1);
+  }
+  strcpy(sip_addr,argv[1]);
+  port=atoi(argv[2]);
+  int sid=socket(AF_INET,SOCK_DGRAM,0);
+  if (sid<0)
+  {
+    printf("Cannot create socket...\n");
+    exit(1);
+  }
+  struct sockaddr_in saddr,caddr;
+  saddr.sin_family=AF_INET;
+  saddr.sin_addr.s_addr=inet_addr(sip_addr);
+  saddr.sin_port=htons(port);
+  if (bind(sid,(struct sockaddr *)&saddr,sizeof(saddr))<0)
+  {
+    printf("Cannot bind to the server...\n");
+    close(sid);
+    exit(1);
+  }
+  printf("| Server Online |\n");
+  while(1)
+  {
+    struct message_struct message;
+    int32_t len=sizeof(caddr);
+    recvfrom(sid,(void *)&message,sizeof(message),0,(struct sockaddr *)&caddr,&len);
+    if (strcmp(message.dataword,"end")==0)
+    {
+      printf("| Server Offline |\n");
+      close(sid);
+      exit(1);
+    }
+    printf("\nDataword : %s\n",message.dataword);
+    printf("Divisor : %s\n",message.divisor);
+    CRC(message.dataword,message.divisor,message.codeword,message.remainder);
+    sendto(sid,(void *)&message,sizeof(message),0,(struct sockaddr *)&caddr,sizeof(caddr));
+  }
+}
